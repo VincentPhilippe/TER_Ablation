219d41f541211a43e14f203e4907dbe932d65a89
Lithiox <thennion40@gmail.com>
2018-03-07 17:46:33 +0100

    Bonjour, cc'est la structure du programme

---
+++ main.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  /* code */
+
+  /*
+  Lire paramètres .dat
+  */
+
+  /*
+  Afficher menu
+  */
+
+  /*
+  Création maillage
+  */
+
+  /*
+  Initialisation concentration et surface
+  +Sauvegarde
+  */
+
+// Boucle en temps
+
+  /*
+  Résolution équation de diffusion
+  */
+
+  /*
+  Calcul de variation de hauteur de la surface
+  */
+
+  /*
+  Reconstruction de la surface
+  */
+
+  /*
+  Sauvegarde de la concentration et de la surface
+  */
+
+// Fin de la boucle
+
+  return 0;
+}
