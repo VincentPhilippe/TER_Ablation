5980c5d792f06a370ea0167b8ba4a0014d96c816
Lithiox <thennion40@gmail.com>
2018-03-13 17:41:36 +0100

    Bonjour, la suite de la diffusion

--- diffusion.cpp
+++ diffusion.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-#include "dense"
+#include "Dense"
 #include "maillage.h"
 #include "read_data.h"
 #include "rebuild_surface.h"
@@ -12,6 +12,21 @@ diffusion::diffusion(Maillage2DCarre& maillage, ReadData& data)
 {
   _maillage = maillage;
   _data = data;
+  _surface = surface;
 
- _ninterf = data.Get
+  _concentration = data.GetC0();
+  _vitesse = MatrixXd::Zero(maillage.GetNx());
+}
+
+void diffusion::resolution() //Résolution de dC/dt = d2C/dx2
+{
+  double dt = 0.4*dx, erreur = 10;
+  int n;
+  MatrixXd C1;
+  C1 = MatrixXd::Zero(maillage.GetNz(), maillage.GetNx());
+
+  while(erreur > 1e-5, n < 100000)
+  {
+    
+  }
 }
