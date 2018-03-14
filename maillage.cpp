ea0be934aadd789a3bece919811524751c1d1698
Gigamax179 <maxime.largeaud@gmail.com>
2018-03-14 13:46:56 +0100

    Yo !
    G corrigé le mayaj

--- maillage.cpp
+++ maillage.cpp
@@ -3,34 +3,34 @@
 using namespace Eigen;
 
 //constructeur par défaut
-void Maillage2DCarre()
+Maillage2DCarre::Maillage2DCarre()
 {
-  _name = default;
+  _name = "default";
   _deltaX = 0.1;
   _deltaZ = 0.1;
   _Lx = 10;
   _Lz = 10;
 
   int _nbX = _Lx/_deltaX;
-  int _nbZ = _Lz/_deltaZ
+  int _nbZ = _Lz/_deltaZ;
 
   _coordX.resize(_nbX);
   _coordZ.resize(_nbZ);
 
   for (int i = 0 ; i < _nbX ; i++)
   {
-    _coordX(i) = deltaX*i + deltaX/2;
+    _coordX(i) = _deltaX*i + _deltaX/2;
   }
 
   for (int i = 0 ; i < _nbZ ; i++)
   {
-    _coordZ(i) = deltaZ*i + deltaZ/2;
+    _coordZ(i) = _deltaZ*i + _deltaZ/2;
   }
 
-}
+};
 
 //constructeur
-void Maillage2DCarre(std::string name, double deltaX, double deltaZ, double _Lx, double _Lz)
+Maillage2DCarre::Maillage2DCarre(std::string name, double deltaX, double deltaZ, double Lx, double Lz)
 {
   _name = name;
   _deltaX = deltaX;
@@ -39,7 +39,7 @@ void Maillage2DCarre(std::string name, double deltaX, double deltaZ, double _Lx,
   _Lz = Lz;
 
   int _nbX = _Lx/_deltaX;
-  int _nbZ = _Lz/_deltaZ
+  int _nbZ = _Lz/_deltaZ;
 
   _coordX.resize(_nbX);
   _coordZ.resize(_nbZ);
@@ -54,4 +54,7 @@ void Maillage2DCarre(std::string name, double deltaX, double deltaZ, double _Lx,
     _coordZ(i) = deltaZ*i + deltaZ/2;
   }
 
-}
+};
+
+//destructeur
+Maillage2DCarre::~Maillage2DCarre(){};
--- maillage.h
+++ maillage.h
@@ -6,25 +6,22 @@ class Maillage2DCarre
     // nom du maillage
     std::string _name;
     // taille du maillage et raffinement
-    int _deltaX, _deltaZ, _Lz, _Lx;
+    double _deltaX, _deltaZ, _Lz, _Lx;
     // vecteurs coordonnées
     Eigen::VectorXd _coordX, _coordZ;
 
     // C'est Thomas tu peux rajouter Nx et Nz dans les attributs et mettre une méthode GetNx / GetNz
 
-  public: // UN CONSTRUCTEUR N'A PAS DE VOID DEVANT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  public:
     // constructeur par défaut
     Maillage2DCarre();
     // constructeur 1
     Maillage2DCarre(std::string name, double deltaX, double deltaZ, double _Lx, double _Lz);
     // destructeur
-<<<<<<< HEAD
-    void ~Maillage2DCarre();
+    ~Maillage2DCarre();
 
     //fonctions d'accès aux variables de l'objet
-    VectorXd GetCoordX(){return _coordX};
-    VectorXd GetCoordZ(){return _coordZ};
-=======
-    ~Maillage2DCarre();
->>>>>>> c30fd3124a7a10c8d1140e94860c3999674986f8
-}
+    Eigen::VectorXd GetCoordX(){return _coordX;};
+    Eigen::VectorXd GetCoordZ(){return _coordZ;};
+
+};
