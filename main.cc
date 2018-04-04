b3858514476c9bb6f9637ab1361866d2cae55f5b
Gigamax179 <maxime.largeaud@gmail.com>
2018-04-04 18:13:43 +0200

    Construction du fichier main

--- main.cc
+++ main.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "diffusion.h"
 #include "maillage.h"
 #include "read_data.h"
 
@@ -17,26 +18,32 @@ int main(int argc, char** argv)
   // --------------------------- Fichier de données --------------------------------/
   read_data* data_file = new read_data(data_file_name);
   data_file->read_datafile();
-  //------- /
 
-  /* code */
+  double dx = data_file->Get_dx();
+  double dz = data_file->Get_dz(); //une petite fonction pour get dy serait pas mal dans le cas 3D :3
+  double Lx = data_file->Get_Lx();
+  double Lz = data_file->Get_Lz();
+  double Nx = data_file->Get_Nx();
+  double dt = data_file->Get_dt();
+  double tfinal = data_file->Get_tfinal();
+  string file_name= data_file->Get_file_name();
 
-  /*
-  Lire paramètres .dat
-  */
-
-  /*
-  Afficher menu
-  */
+  cout << "------------------- LECTURE DES DONNEES -------------------" << endl;
+  cout << "Simulation -- Recul d'une surface composite"                 << endl;
+  cout << "Fichier source : " << file_name                              << endl;
+  cout << "Pas de temps dt = " << dt                                    << endl;
+  cout << "Temps final Tmax = " << tfinal                               << endl;
 
   // -------------------------- Création du maillage -------------------------------/
-  Cartesien2D maillage("maillage_1", 0.1, 0.1, 10., 10.);
-  // ------- /
+  Cartesien2D maillage("Maillage 2D", dx, dz, Lx, Lz);
+  cout << "----------------------- MAILLAGE -----------------------" << endl;
+  cout << maillage.GetMeshName() << dx                               << endl;
+  cout << "Pas d'espace dx =" << dx                                  << endl;
+  cout << "Pas d'espace dz = " << dz                                 << endl;
+  cout << "Nombre de noeuds = " << maillage.GetNx()*maillage.GetNz() << endl;
 
-  /*
-  Initialisation concentration et surface
-  +Sauvegarde
-  */
+  // ----------------------------- Initialisation ----------------------------------/
+  diffusion plateau(*data_file, maillage);
 
 // Boucle en temps
 
