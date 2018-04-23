5527e9e6168eb43c427569dd1fb05ad23b6f841e
Theo Jeanneau <theo.jeanneau@enseirb-matmeca.fr>
2018-04-23 19:22:25 +0200

    h

--- plic3D.h
+++ plic3D.h
@@ -1,21 +1,22 @@
-#ifndef PLIC_H
-#define PLIC_H
+#ifndef PLIC3D_H
+#define PLIC3D_H
 
 #include "objet.h"
 #include <vector>
 #include <string>
 #include "Sparse"
 #include "Dense"
-#include "recul.h"
+#include "recul3D.h"
 #include "read_data.h"
 
 class plic {
   public:
   plic();
   ~plic();
-  void update(recul *recul){_recul=recul;};
-  double grad_x(const int i, const int j);
-  double grad_y(const int i, const int j);
+  void update(recul3D *recul3D){_recul3D=recul3D;};
+  double grad_x(const int i, const int j, const int k);
+  double grad_y(const int i, const int j, const int k);
+  double grad_z(const int i, const int j, const int k);
   void interf();
   void SaveSol(int n);
   const Eigen::MatrixXd Get_interface() const {return _interface;};
@@ -23,13 +24,13 @@ class plic {
   const Eigen::MatrixXd Get_ninterface() const {return _ninterf;};
 
   private:
-  recul *_recul;
+  recul3D *_recul3D;
   read_data *_read_data;
   Eigen::MatrixXd _phi;
   double p,nx,nxx,ny;
   int _pointsupl,lon,lar,num,k,l;
   int nbtri,nbquad,nbpenta;
-  double dx,dz;
+  double dx,dy,dz;
   //std::vector<std::vector<int> > _interface;   //_inteface[i][j,ax,ay,bx,by]
   Eigen::MatrixXd _ninterf, typinterf; //int  //_ninterf(i,j)=k   //numérotes des cases où il y a présence d'interface
   Eigen::MatrixXd _interface; //_interface[k][ax,ay,bx,by], où k est le numéro de la case, les coordonnées sont en locales, 0 en bas à gauche
