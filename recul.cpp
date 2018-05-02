392be695c519696f9f8e69dea4f78f5dcb8cb198
Vincent_Philippe <vincent.philippe56@gmail.com>
2018-05-02 14:49:53 +0200

    recul.cpp
    affichage off

--- recul.cpp
+++ recul.cpp
@@ -9,7 +9,7 @@ recul::recul(read_data &_data)
   //_read_data=&_data;
   //_diff=0;
   _C_solide=_read_data.Get_C_Solide();
-  cout << _C_solide << endl;
+  //cout << _C_solide << endl;
   _dtmax=_read_data.Get_dt();
   _dt=_dtmax;
   _dx=_read_data.Get_dx();
@@ -30,9 +30,9 @@ void recul::recul_surface()
   _interface=_plic->Get_interface();
   _vitesse=_diff->GetVitesse();
   int affichage;
-  affichage=1;
+  affichage=0;
 
-  cout << "_C_solide" << endl << _C_solide << endl;
+  //cout << "_C_solide" << endl << _C_solide << endl;
   if (affichage==1) {
     cout << "_ninterf" << endl << _ninterf << endl;
     cout << "_interface" << endl << _interface << endl;
