5be0defec1fcb663ae345c1c55a4cb0f02cc1e1f
Pavote <36933933+Pavote@users.noreply.github.com>
2018-05-03 21:26:04 +0200

    décallage indice

--- recul.cpp
+++ recul.cpp
@@ -57,22 +57,22 @@ void recul::recul_surface()
 
       k = _ninterf(i,j);
 
-      if (k>0) {
+      if (k>=0) {
         //cout << "k " << k << endl;
         //calcul de l'angle alpha
         double xa, za, xb, zb, t_alpha, alpha;
 
         //repere local
-        if (_interface(k-1,0)<_interface(k-1,2)) {
-          xa = _interface(k-1,0);
-          za = _interface(k-1,1);
-          xb = _interface(k-1,2);
-          zb = _interface(k-1,3);
+        if (_interface(k,0)<_interface(k,2)) {
+          xa = _interface(k,0);
+          za = _interface(k,1);
+          xb = _interface(k,2);
+          zb = _interface(k,3);
         } else {
-          xb = _interface(k-1,0);
-          zb = _interface(k-1,1);
-          xa = _interface(k-1,2);
-          za = _interface(k-1,3);
+          xb = _interface(k,0);
+          zb = _interface(k,1);
+          xa = _interface(k,2);
+          za = _interface(k,3);
         }
 
         /*xa = -j*_dx + _interface(0,k-1);
@@ -1105,12 +1105,12 @@ void recul::cpositive()
     for (int j = 0; j <_nx; j++) {
       if (_C_solide(i,j)<=0.00000000001) {
         _C_solide(i,j)=0;
-        _ninterf(i,j)=0;
+        _ninterf(i,j)=-2;
       } else if (_C_solide(i,j)>=1) {
         _ninterf(i,j)=-1;
       } else {
-        _nbinterface=_nbinterface+1;
         _ninterf(i,j)=_nbinterface;
+        _nbinterface=_nbinterface+1;
       }
     }
   }
